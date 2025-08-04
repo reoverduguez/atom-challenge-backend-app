a78a2708ef2e358286a07023dd6f872e5695c80f
reoverduguez <reoverduguez@gmail.com>
2025-08-04 01:03:07 -0400

    adjusting cors

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -3,11 +3,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-console.log('CORS_ORIGIN', process.env.CORS_ORIGIN);
-
 export const corsOptions: cors.CorsOptions = {
   origin: [process.env.CORS_ORIGIN ?? 'localhost'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
+  credentials: false,
 };
