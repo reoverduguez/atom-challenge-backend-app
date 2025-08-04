23f35c1ecd582b0b142708b32feb0403a8d4de99
reoverduguez <reoverduguez@gmail.com>
2025-08-04 00:08:45 -0400

    Using dynamic PORT to bind server to

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,7 @@ app.use(express.json());
 app.use('/auth', authRouter);
 app.use('/task', taskRouter);
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000; // Fallback to 3000 for local dev
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
